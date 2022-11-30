767f42fbe0327cd4f58d71eed3134935c7581410
Badri <9299547077@mail.ru>
2022-11-30 17:35:07 +0300

    Чётное число

---
+++ Homework_6/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//  4 -> да
+
+System.Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(System.Console.ReadLine());
+
+  if (numberA % 2 == 0)
+  {
+    System.Console.WriteLine("Чётное число");
+  }
+
+  else
+  {
+    System.Console.WriteLine("Нечётное число");
+  }
+
+
